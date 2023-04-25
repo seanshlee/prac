88699ce9e49a65eb78a7f8337778ecef1ed158f7
iamyanghee <yanghee.data@gmail.com>
2023-04-25 19:25:43 +0900

    Get dump from csv to db directly - Complete methods

--- 1.get_dump.py
+++ 1.get_dump.py
@@ -56,12 +56,37 @@ class Database :
         self.closeDB()
 
     def createRawData(self) : 
-        query = ''
+        query = 'create table raw_data ('
+        query += 'id                        int unsigned primary key auto_increment'
+        query += ',unnamed                  int'
+        query += ',parent_marital_status    varchar(255)'
+        query += ',parent_educ              varchar(255)'
+        query += ',is_first_child           varchar(255)'
+        query += ',nr_siblings              float'
+        query += ',gender                   varchar(255)'
+        query += ',ethnic_group             varchar(255)'
+        query += ',transport_means          varchar(255)'
+        query += ',lunch_type               varchar(255)'
+        query += ',practice_sport           varchar(255)'
+        query += ',wkly_study_hours         varchar(255)'
+        query += ',test_prep                varchar(255)'
+        query += ',math_score               int'
+        query += ',reading_score            int'
+        query += ',writing_score            int'
+        query += ',created_datetime         datetime default now()'
+        query += ',updated_datetime         datetime on update now()'
+        query += ')'
         self.create(query)
 
     def insertRawData(self,dt:pd.DataFrame) : 
-        query = ''
-        self.insert(query)
+        for idx, row in dt.iterrows() :
+            query = 'insert into raw_data ('
+            query += 'unnamed,parent_marital_status,parent_educ,is_first_child,nr_siblings,gender,ethnic_group,transport_means,lunch_type,practice_sport,wkly_study_hours,test_prep,math_score,reading_score,writing_score'
+            query += ')'
+            query += ' values ('
+            query += ','.join(['null' if pd.isna(row[col]) else f'"{row[col]}"' for col in dt.columns])
+            query += ')'
+            self.insert(query)
 
 
 path = './data/'
@@ -81,5 +106,9 @@ columns_ordered = [
 data = data.reindex(columns=columns_ordered)
 
 db = Database()
-db.createEngine()
-db.toSql(data)
+
+# db.createEngine()
+# db.toSql(data)
+
+db.createRawData()
+db.insertRawData(data)
